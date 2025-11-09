9f1e198221f3457cd424d1d3746f6662a8a56175
Dietrich Daroch <Dietrich@Daroch.me>
2025-11-09 20:13:56 -0300

    Add raspberry pi3 sd card image

--- Justfile
+++ Justfile
@@ -1,6 +1,8 @@
-
 build_iso:
 	nix build .#nixosConfigurations.live_iso.config.system.build.isoImage
+
+build_raspberry_pi3:
+	nix build .#nixosConfigurations.raspberry_pi3.config.system.build.images.sd-card
 
 boot_iso: build_iso
 	nix-shell -p qemu --command "qemu-system-x86_64 -enable-kvm -m 256 -cdrom result/iso/nixos-*.iso"
@@ -8,5 +10,8 @@ boot_iso: build_iso
 list_drives:
 	ls -l /dev/sd*
 
-write TARGET: build_iso
+write_iso TARGET: build_iso
 	sudo dd if=$(find result/ -name '*.iso') of={{TARGET}} bs=4M status=progress conv=fdatasync
+
+write_raspberry_pi3_sd_image TARGET: build_raspberry_pi3
+	sudo dd if=$(find result/ -name 'nixos-image-sd-card-*.img.zst') of={{TARGET}} bs=4M status=progress conv=fdatasync
--- flake.nix
+++ flake.nix
@@ -31,6 +31,56 @@
 
     {
       nixosConfigurations = {
+        "raspberry_pi3" = nixpkgs.lib.nixosSystem {
+          system = "aarch64-linux";
+          modules = [
+            nixos-hardware.nixosModules.raspberry-pi-3 # ~/Projects/nixos-hardware/raspberry-pi/3/default.nix
+            nixos-hardware.nixosModules.common-pc-ssd # ~/Projects/nixos-hardware/common/pc/ssd/default.nix
+
+            ./personal # ./personal/default.nix
+
+            id.users # ~/Projects/id/users/default.nix
+
+            # SystemN  ~/Projects/systemn/flake.nix
+            # =======
+
+            # Hardware
+            systemn.nixosModules.hardware-bluetooth # ~/Projects/systemn/hardware/bluetooth.nix
+            # systemn.nixosModules.hardware-graphics # ~/Projects/systemn/hardware/graphics/default.nix
+            systemn.nixosModules.hardware-keyboard-ergodox_ez # ~/Projects/systemn/hardware/keyboard/ergodox_ez.nix
+            systemn.nixosModules.hardware-memtest # ~/Projects/systemn/hardware/memtest.nix
+            # systemn.nixosModules.hardware-printing # ~/Projects/systemn/hardware/printing.nix
+            systemn.nixosModules.hardware-truerng # ~/Projects/systemn/hardware/truerng.nix
+
+            # System
+            systemn.nixosModules.system # ~/Projects/systemn/system/default.nix
+            systemn.nixosModules.system-base-hardware_information # ~/Projects/systemn/system/base/hardware_information.nix
+            systemn.nixosModules.system-base-watchdog # ~/Projects/systemn/system/base/watchdog.nix
+            systemn.nixosModules.system-input-mouse # ~/Projects/systemn/system/input/mouse.nix
+            systemn.nixosModules.system-input-keyboard-dvorak # ~/Projects/systemn/system/input/keyboard/dvorak.nix
+            systemn.nixosModules.system-networking # ~/Projects/systemn/system/networking/default.nix
+            systemn.nixosModules.system-networking-connectivity # ~/Projects/systemn/system/networking/connectivity/default.nix
+
+            # Desktop
+            systemn.nixosModules.desktop # ~/Projects/systemn/desktop/default.nix
+
+            (
+              {
+                lib,
+                pkgs,
+                ...
+              }:
+
+              {
+                # Generated with mkpasswd --method=sha-512
+                users.users.root.initialHashedPassword = lib.mkForce "$6$dyINLcIm1E3tQ5xU$nSREiDQrPVb9hK/Rncw24vxJr/gZnSMeDRe2vOGTH3yfgeHMJwr2Sq3rrA1J.O9N0dEvpFCI0F0Q5qhc1HMpF0";
+                # Enable SSH in the boot process.
+                systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
+              }
+            )
+          ];
+        };
+
         "live_iso" = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
